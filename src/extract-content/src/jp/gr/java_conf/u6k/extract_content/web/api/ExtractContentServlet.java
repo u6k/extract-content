d048dabb6702c7be12a85c4df776c8c661e963bf
u6k Apps <u6k.apps@gmail.com>
2014-08-20 18:14:28 +0900

    文字化けの原因究明のため、取得したHTMLをデバッグログ出力するようにしました。

--- src/extract-content/src/jp/gr/java_conf/u6k/extract_content/web/api/ExtractContentServlet.java
+++ src/extract-content/src/jp/gr/java_conf/u6k/extract_content/web/api/ExtractContentServlet.java
@@ -56,6 +56,9 @@ public class ExtractContentServlet extends HttpServlet {
                     bout.write(buf, 0, len);
                 }
 
+                LOG.info("none:" + bout.toString());
+                LOG.info("UTF-8:" + bout.toString("UTF-8"));
+
                 html = bout.toString();
             } finally {
                 in.close();
