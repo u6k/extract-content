3ad44bd4886a0111ca2f56e8799f5d84fed3724d
u6k <u6k.apps@gmail.com>
2017-09-13 16:30:41 +0900

    設定情報を整理しました。

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import app
 
 if __name__ == "__main__":
     app.debug=True
-    app.config.from_object("app.settings")
+    app.config.from_object("config.BaseConfig")
     app.run(host="0.0.0.0")
